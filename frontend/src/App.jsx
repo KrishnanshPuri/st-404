6039860353cb08fdbd89afc70c7f8e39065bc068
Atharv Dubey <athdubey120904@gmail.com>
2026-02-21 18:59:41 +0530

    added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
     <>
+    <Navbar / >
      <p className="bg-red-700">Hello</p>
     </>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='sticky top-0 w-full h-14 bg-blue-700'>
+
+    </div>
+  )
+}
+
+export default Navbar
